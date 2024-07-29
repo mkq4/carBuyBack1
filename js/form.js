349b5858636352605ab03f52fba836e91ec2a8d6
Egor <egordelovayakolbasa@yandex.ru>
2024-07-29 22:27:17 +0300

    jkfdngmdfg

--- js/form.js
+++ js/form.js
@@ -1,24 +1,44 @@
-document.getElementById('carForm').addEventListener('submit', sendContactTelegram);
-document.getElementById('contactForm').addEventListener('submit', sendContactTelegram);
-
+document.getElementById('carForm').addEventListener('submit', sendCarForm);
+document.getElementById('contactForm').addEventListener('submit', sendContactForm);
+const result = document.querySelectorAll('.form-car__form-result')
 
 const token = '5862490675:AAG7nuV9jxYlbWnk8OuxqabSvSAFdtwTeSY';
 const chatId = '-4218623363';
 const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
-async function sendContactTelegram(event) {
+async function sendCarForm(event) {
     event.preventDefault();
 
-    let phone = document.getElementById('phone_form').value;
-    let name = document.getElementById('name_form').value;
-    let car = document.getElementById('car_form').value;
-    let mileage = document.getElementById('mileage_form').value;
-    let message = `Заявка на выкуп авто\nТелефон: ${phone}\nИмя: ${name}\nМашина: ${car}\nПробег: ${mileage}`;
-    if(car === '' && mileage === '') {
-        phone = document.getElementById('phone').value;
-        name = document.getElementById('name').value;
-        message = `Заявка на обратный звонок\nТелефон: ${phone}\nИмя: ${name}`;
-    } 
+    const phone = document.getElementById('phone_form').value;
+    const name = document.getElementById('name_form').value;
+    const car = document.getElementById('car_form').value;
+    const mileage = document.getElementById('mileage_form').value;
+
+    if (!phone || !name || !car || !mileage) {
+        alert('Пожалуйста, заполните все поля формы.');
+        return; 
+    }
+
+    const message = `Заявка на выкуп авто\nТелефон: ${phone}\nИмя: ${name}\nМашина: ${car}\nПробег: ${mileage}`;
+
+    await sendTelegramMessage(message);
+    document.getElementById('carForm').reset();
+}
+
+async function sendContactForm(event) {
+    event.preventDefault();
+
+    const phone = document.getElementById('phone').value;
+    const name = document.getElementById('name').value;
+
+
+    const message = `Заявка на обратный звонок\nТелефон: ${phone}\nИмя: ${name}`;
+
+    await sendTelegramMessage(message);
+    document.getElementById('contactForm').reset();
+}
+
+async function sendTelegramMessage(message) {
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -32,10 +52,7 @@ async function sendContactTelegram(event) {
         });
 
         if (response.ok) {
-            // alert('Заявка отправлена');
-            console.log(message)
-            console.log(name, phone, car, mileage)
-            document.getElementById('carForm').reset();
+            result.forEach(el => el.innerHTML = "заявка отправлена")
         } else {
             alert('Ошибка при отправке заявки');
             console.error('Response status:', response.status);
@@ -45,4 +62,3 @@ async function sendContactTelegram(event) {
         alert('Ошибка при отправке заявки');
     }
 }
-
--- js/main.js
+++ js/main.js
@@ -1,68 +1,24 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const openPopupButton = document.getElementById('openPopup');
-    const popup = document.getElementById('popup');
-    const closePopupButton = document.getElementById('closePopup');
-    const sliderButton = document.getElementById("slider-button")
-    document.querySelector('form').addEventListener('submit', sendEmailTelegram);
-    // Открытие попапа
-    sliderButton.addEventListener('click', function() {
-        popup.style.display = 'block';
-    });
-    openPopupButton.addEventListener('click', function() {
-        popup.style.display = 'block';
-    });
-    // Закрытие попапа
-    closePopupButton.addEventListener('click', function() {
-        popup.style.display = 'none';
-    });
-    // Закрытие попапа при клике вне его
-    window.addEventListener('click', function(event) {
-        if (event.target === popup) {
-            popup.style.display = 'none';
-        }
-    });
 
-    async function sendEmailTelegram(event) {
-        event.preventDefault();
-    
-        const phone = document.getElementById('phone_form').value;
-        const name = document.getElementById('name_form').value;
-        const car = document.getElementById('car_form').value;
-        const mileage = document.getElementById('mileage_form').value;
-    
-        const message = `Заявка на выкуп авто\nТелефон: ${phone}\nИмя: ${name}\nМашина: ${car}\nПробег: ${mileage}`;
-    
-        const token = '5862490675:AAG7nuV9jxYlbWnk8OuxqabSvSAFdtwTeSY';
-        const chatId = '1059407854';
-    
-        const url = `https://api.telegram.org/bot${token}/sendMessage`;
-    
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    chat_id: chatId,
-                    text: message
-                })
-            });
-            console.log('Phone:', phone);
-            console.log('Name:', name);
-            console.log('Car:', car);
-            console.log('Mileage:', mileage);
-            if (response.ok) {
-                alert('Заявка отправлена');
-            } else {
-                alert('Ошибка при отправке заявки');
-                console.error('Response status:', response.status);
-            }
-        } catch (error) {
-            console.error('Fetch error:', error);
-            alert('Ошибка при отправке заявки');
-        }
-    }
-
+const openPopupButton = document.getElementById('openPopup');
+const popup = document.getElementById('popup');
+const closePopupButton = document.getElementById('closePopup');
+const sliderButton = document.getElementById("slider-button")
+// Открытие попапа
+sliderButton.addEventListener('click', function() {
+    popup.style.display = 'block';
 });
+openPopupButton.addEventListener('click', function() {
+    popup.style.display = 'block';
+});
+// Закрытие попапа
+closePopupButton.addEventListener('click', function() {
+    popup.style.display = 'none';
+});
+// Закрытие попапа при клике вне его
+window.addEventListener('click', function(event) {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
+
