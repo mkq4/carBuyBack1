349da2a88407ba7a7162ef28bd9aa38bc77eb0a4
Egor <egordelovayakolbasa@yandex.ru>
2024-07-31 02:05:18 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 // script.js
 
-const openPopupButton = document.getElementById('openPopup');
-const popup = document.getElementById('popup');
-const closePopupButton = document.getElementById('closePopup');
-const sliderButton = document.getElementById("slider-button")
-// Открытие попапа
-sliderButton.addEventListener('click', function() {
-    popup.style.display = 'block';
-});
-openPopupButton.addEventListener('click', function() {
-    popup.style.display = 'block';
-});
-// Закрытие попапа
-closePopupButton.addEventListener('click', function() {
-    popup.style.display = 'none';
-});
-// Закрытие попапа при клике вне его
-window.addEventListener('click', function(event) {
-    if (event.target === popup) {
-        popup.style.display = 'none';
-    }
-});
 
+document.addEventListener('DOMContentLoaded', () => {
+    const openPopupButton = document.getElementById('openPopup');
+    const popup = document.getElementById('popup');
+    const closePopupButton = document.getElementById('closePopup');
+    const sliderButton = document.getElementById("slider-button")
+    const burgerIcon = document.getElementById('burgerIcon');
+    const burgerMenu = document.getElementById('burgerMenu');
+    const closeBurger = document.getElementById('closeBurger');
+    
+    burgerIcon.addEventListener('click', () => {
+        burgerMenu.style.display = 'flex';
+    });
+    
+    closeBurger.addEventListener('click', () => {
+        burgerMenu.style.display = 'none';
+    });
+
+    // Открытие попапа
+    sliderButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+    openPopupButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+    // Закрытие попапа
+    closePopupButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+    // Закрытие попапа при клике вне его
+    window.addEventListener('click', function(event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+
+
+});
